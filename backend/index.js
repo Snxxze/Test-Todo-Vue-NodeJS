fb7d215411b402e495b8483aa2b512c1dbd3ec76
Snxxze <keng3772@gmail.com>
2025-10-28 14:44:04 +0700

    add PostgreSQL conn using Prisma

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,68 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
+
 app.use(cors());
 app.use(express.json());
 
-let todos = [];
-
+// test api
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok", time: new Date().toISOString() });
 });
 
-app.get("/api/todos", (req, res) => {
-  res.json(todos);
+app.get("/api/todos", async (req, res) => {
+  try {
+    const result = await prisma.todo.findMany({ orderBy: { id: "desc" } });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
-app.post("/api/todos", (req, res) => {
+app.post("/api/todos", async (req, res) => {
   const title = (req.body?.title || "").trim();
   if (!title) return res.status(400).json({ error: "title required" });
-  const todo = { id: Date.now(), title, done: false };
-  todos.push(todo);
-  res.status(201).json(todo);
+  try {
+    const result = await prisma.todo.create({ data: {title} });
+    res.status(201).json(result)
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
-app.put("/api/todos/:id", (req, res) => {
+app.put("/api/todos/:id", async (req, res) => {
   const id = Number(req.params.id);
-  const t = todos.find((x) => x.id === id);
-  if (!t) return res.status(404).json({ error: "not found" });
-  t.done = !t.done;
-  res.json(t);
+  try {
+    const current = await prisma.todo.findUnique({ where: {id}});
+    if (!current) return res.status(404).json({ error: "not found" });
+    const updated = await prisma.todo.update({
+      where: {id},
+      data: {done: !current.done },
+    });
+    res.json(updated);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error "});
+  }
 });
 
-app.delete("/api/todos/:id", (req, res) => {
+app.delete("/api/todos/:id", async (req, res) => {
   const id = Number(req.params.id);
-  const i = todos.findIndex((x) => x.id === id);
-  if (i === -1) return res.status(404).json({ error: "not found" });
-  todos.splice(i, 1);
-  res.status(204).send();
+  try {
+    await prisma.todo.delete({where: {id}});
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({ error: "not found" });
+  }
 });
 
 const PORT = process.env.PORT || 8000;
